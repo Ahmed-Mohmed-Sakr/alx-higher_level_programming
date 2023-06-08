7bf187118bc2c9b038114c23614729c69d918ee9
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-06-08 06:02:58 -0700

    make it in two lines

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import builtins; getattr(builtins, '__import__')('os').write(1, b'#pythoniscool\n')
+import builtins; getattr(builtins, '__import__')('os').write(1, b'#pythoniscool\n')
