9d9509b79ebb2ec8bfef325e4807884068f0aabb
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-06-15 04:04:20 -0700

    Write a function that prints a matrix of integers.

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    row_len = 0
+    for row in matrix:
+        row_len = len(row)
+        for item in row:
+            if (row_len > 1):
+                print("{:d}".format(item), end = " ")
+            else:
+                print("{:d}".format(item), end = "")
+            row_len -= 1
+        print()
