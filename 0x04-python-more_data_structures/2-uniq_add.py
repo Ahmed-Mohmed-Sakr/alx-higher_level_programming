70a64038095799c92d506ab72ca2c2a10db85039
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-06-16 01:22:29 -0700

    fix not imported module

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,4 +3,8 @@
 def uniq_add(my_list=[]):
 
     uniqe = {i for i in my_list}
-    return reduce(lambda a, b: a+b, uniqe)
+    list_sum = 0
+    for i in uniqe:
+        list_sum += i
+
+    return list_sum
